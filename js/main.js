e925c2a44a930f6974e31aa64ebb7ac9344310ad
Andrew Stokes <84.andrew.stokes@gmail.com>
2020-04-30 12:03:51 -0500

    Fixed HTTP request error by updating to HTTPS

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ $(function () {
 
 function getData() {
     // Get search results from GIPHY API and loop through items in array, appending each to displayed results
-    $.get("http://api.giphy.com/v1/gifs/search?q=" + $('.search').val() + "+&api_key=8i7DkkKJ0SiHMEDMfoceSri0ATmQmmGw&limit=30").done(function (response) {
+    $.get("https://api.giphy.com/v1/gifs/search?q=" + $('.search').val() + "+&api_key=8i7DkkKJ0SiHMEDMfoceSri0ATmQmmGw&limit=30").done(function (response) {
         for (i in response.data) {
             $('.results').append("<img src ='" + response.data[i].images.original.url + "' style='width:250px;'/>");
         }
